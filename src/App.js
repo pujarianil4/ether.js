633b2f2c05e700bdef4b0224bb4807fd03d4251c
Pujari Anil Dilip INPU <Anil_Dilip.Pujari@syngenta.com>
2022-01-01 13:22:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Import from './components/Import';
+import { ethers } from 'ethers'
+import React from 'react';
+function App() {
+  const [show, setShow] = React.useState(false);
+  const [wallet, setWallet] = React.useState(null);
+  const handleAccount = () => {
+    const wallet = ethers.Wallet.createRandom()
+
+    console.log(wallet);
+    setWallet(wallet);
+  }
+  return (
+    <div className="App">
+      <button onClick={()=> setShow(true)}>IMPORT</button>
+      <button onClick={handleAccount}>CREATE WALLET </button>
+
+    <Import wallet ={wallet} show={show}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Import.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { ethers } from "ethers";
+export default function Import({ wallet, show }) {
+  const [input, setInput] = React.useState("");
+  const [balance, setBalance] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [walletP, setWallet] = React.useState(null);
+  const [isLoading, setLoading] = React.useState(false);
+  let provider = ethers.getDefaultProvider();
+
+  const handleImport = async () => {
+    const walletPrivate = new ethers.Wallet(input);
+    setWallet(walletPrivate);
+  };
+
+  const getWalletDetails = async (walletPrivate) => {
+    setLoading(true);
+    try {
+      let wallet = await walletPrivate.connect(provider);
+      setAddress(wallet.address);
+      let balanceHex = await wallet.getBalance();
+      let balance = ethers.utils.formatUnits(balanceHex);
+      setBalance(balance);
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    if (walletP) {
+      getWalletDetails(walletP);
+    } else {
+        setWallet(wallet)
+    }
+  }, [walletP, wallet]);
+  return (
+    <>
+      {isLoading ? (
+        <div>Loading....</div>
+      ) : (
+        <div>
+          {show && (
+            <>
+              <input type="text" onChange={(e) => setInput(e.target.value)} />
+              <button onClick={handleImport}>IMPORT</button>
+            </>
+          )}
+          {address && balance && (
+            <div>
+              <h1>Address: {address}</h1>
+              <h1>Balance: {balance} ETH</h1>
+              { !show&&<h1>Private Key : {walletP.privateKey}</h1>}
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
+}
